c8ab38237b193f5bb7e92446d8fd0f67ac285e4b
Bijay Jha <bijayjha12345@gmail.com>
2025-11-25 10:32:44 +0545

    Changes

--- 02_Styling_with_CSS/CSS_Styling/src/Button.jsx
+++
@@ -1,6 +0,0 @@
-
-function Button(){
-    return(<button className="button">Subscribe</button>);
-}
-
-export default Button
---
+++ 02_Styling_with_CSS/CSS_Styling/src/Button/Button.jsx
@@ -0,0 +1,10 @@
+
+import styles from './Button.module.css'
+
+
+
+function Button(){
+    return(<button className={styles.button}>Subscribe</button>);
+}
+
+export default Button
