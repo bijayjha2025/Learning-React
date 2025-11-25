68a831a54e73a9a32b91b9b4e1eb1bf0f0868b88
Bijay Jha <bijayjha12345@gmail.com>
2025-11-25 09:14:06 +0545

    Learning Card Components

---
+++ 01_Card_Component/LearningCardComponent/src/Card.jsx
@@ -0,0 +1,16 @@
+
+import dragonPic from './assets/dragon.jpg'
+
+function Card(){
+
+    return(
+        <div className ="card">
+            <img src={dragonPic} alt="Dragon Image"></img>
+            <h2>Dragon</h2>
+            <p>He is a fearsome flying unit capable of attacking both ground and air units, with both high health and damage. Their attacks are ranged and deal splash damage, observed when he attacks enemy troops that overlap or over walls.</p>
+        </div>
+    );
+
+}
+
+export default Card
