172cd414128b50c8db423a35eae7b83193e37a76
Bijay Jha <bijayjha12345@gmail.com>
2025-11-25 09:17:50 +0545

    Giving class names to stylize them

--- 01_Card_Component/LearningCardComponent/src/Card.jsx
+++ 01_Card_Component/LearningCardComponent/src/Card.jsx
@@ -5,9 +5,9 @@ function Card(){
 
     return(
         <div className ="card">
-            <img src={dragonPic} alt="Dragon Image"></img>
-            <h2>Dragon</h2>
-            <p>He is a fearsome flying unit capable of attacking both ground and air units, with both high health and damage. Their attacks are ranged and deal splash damage, observed when he attacks enemy troops that overlap or over walls.</p>
+            <img className= "cardImage" src={dragonPic} alt="Dragon Image"></img>
+            <h2 className="cardTitle">Dragon</h2>
+            <p className="cardDescription">He is a fearsome flying unit capable of attacking both ground and air units, with both high health and damage. Their attacks are ranged and deal splash damage, observed when he attacks enemy troops that overlap or over walls.</p>
         </div>
     );
 
