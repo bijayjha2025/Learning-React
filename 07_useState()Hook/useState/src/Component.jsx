2c7906da58c6a7f5ed51c6a282aedbcfba3daf26
Bijay Jha <bijayjha12345@gmail.com>
2025-11-28 08:15:31 +0545

    Adjustments

--- 07_useState()Hook/useState/src/Component.jsx
+++ 07_useState()Hook/useState/src/Component.jsx
@@ -3,9 +3,18 @@ import { useState } from "react";
 function Component(){
     const [name, setName]= useState();
     const [age, setAge]= useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
-        setName("Bob");
+        setName("Bijay");
+    }
+
+     const incrementAge = () => {
+        setAge(age + 1);
+    }
+
+     const EmployedStatus = () => {
+        setIsEmployed(!isEmployed);
     }
 
     return(
@@ -13,8 +22,11 @@ function Component(){
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
 
-            <p>Name: {age}</p>
+            <p>Age: {age}</p>
             <button onClick={incrementAge}>Increment Age</button>
+
+            <p>Employee Status: {isEmployed ? "Yes" : "No"}</p>
+            <button onClick={EmployedStatus}>Employed</button>
         </div>
     );
 }
