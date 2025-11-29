57e7fbf6d22ac422f211fb1bda9483c629e79811
Bijay Jha <bijayjha12345@gmail.com>
2025-11-29 16:28:38 +0545

    Using updater function to create new object using useState

--- 09_UpdateObjectInState/updaterfunc/src/Component1.jsx
+++ 09_UpdateObjectInState/updaterfunc/src/Component1.jsx
@@ -7,14 +7,29 @@ function Component1(){
                                       brand: "WaiWai",
                                       type: "Packed" });
 
+
+
+    function handlePriceChange(event){
+        setFood(f => ({...f, price: event.target.value}));
+    }
+
+    function handleBrandChange(event){
+        setFood(f => ({...f, brand: event.target.value}));
+    }
+
+    function handleTypeChange(event){
+        setFood(f => ({...f, type: event.target.value}));
+    }
+
+
     return(
         <>
          <div>
             <p>Your favorite food is: {food.price} {food.brand} {food.type}</p>
 
-            <input type="number" value={food.price}/><br></br>
-            <input type= "text" value={food.brand}/><br></br>
-            <input type="text" value={food.type}/><br></br>
+            <input type="number" value={food.price} onChange={handlePriceChange}/><br></br>
+            <input type= "text" value={food.brand} onChange={handleBrandChange}/><br></br>
+            <input type="text" value={food.type} onChange={handleTypeChange}/><br></br>
          </div>;
         </>
     );
